66961e512cfad4a10308e1318b484bd489a345f1
Boris Generalic <boris.generalic@gmail.com>
2018-01-15 19:17:22 +0100

    Added load_weight.sql

---
+++ load_weight.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `weight`;
+CREATE TABLE `weight` (
+  food_id int PRIMARY KEY REFERENCES food(id) NOT NULL,
+  weight_id int PRIMARY KEY NOT NULL,
+  quantity float NOT NULL DEFAULT '',
+  unit text NOT NULL DEFAULT '',
+  eqv float NOT NULL DEFAULT '',
+  manufac_name text NOT NULL DEFAULT '',
+  refuse int NOT NULL
+);
+
+.separator "^"
+.import data/WEIGHT.txt weight
